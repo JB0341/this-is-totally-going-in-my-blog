fae00d00f199b3db83d5d1f38fe9ee686cea01f6
j b <j.is.brown@gmail.com>
2024-05-23 17:05:33 -0500

    javascript for storing/retrieving data using local storage completed

---
+++ assets/javascript/blog.js
@@ -0,0 +1,6 @@
+const divTags = document.querySelectorAll('div');
+const bodyTags = document.querySelectorAll('body');
+const headerTags = document.querySelectorAll('body');
+const pTags = document.getElementById('p');
+
+const recentBlog = JSON.parse(localStorage.getItem('blogs'));
